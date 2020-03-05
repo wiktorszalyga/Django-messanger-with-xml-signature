b1665d5d653cbf0f0ebf8af4f3cf0b2c566973db
Wiktor Szałyga <szalyga.wiktor@gmail.com>
2020-03-05 20:40:41 +0100

    Update x509ceryficat.py

--- src/x509ceryficat.py
+++ src/x509ceryficat.py
@@ -6,7 +6,6 @@ import ipaddress
 
 
 def generate_selfsigned_cert(hostname, ip_addresses=None, key=None):
-    """Generates self signed certificate for a hostname, and optional IP addresses."""
     from cryptography import x509
     from cryptography.x509.oid import NameOID
     from cryptography.hazmat.primitives import hashes
@@ -26,21 +25,16 @@ def generate_selfsigned_cert(hostname, ip_addresses=None, key=None):
         x509.NameAttribute(NameOID.COMMON_NAME, hostname)
     ])
 
-    # best practice seem to be to include the hostname in the SAN, which *SHOULD* mean COMMON_NAME is ignored.
+
     alt_names = [x509.DNSName(hostname)]
 
-    # allow addressing by IP, for when you don't have real DNS (common in most testing scenarios
     if ip_addresses:
         for addr in ip_addresses:
-            # openssl wants DNSnames for ips...
             alt_names.append(x509.DNSName(addr))
-            # ... whereas golang's crypto/tls is stricter, and needs IPAddresses
-            # note: older versions of cryptography do not understand ip_address objects
             alt_names.append(x509.IPAddress(ipaddress.ip_address(addr)))
 
     san = x509.SubjectAlternativeName(alt_names)
 
-    # path_len=0 means this cert can only sign itself, not other certs.
     basic_contraints = x509.BasicConstraints(ca=True, path_length=0)
     now = datetime.utcnow()
     cert = (
